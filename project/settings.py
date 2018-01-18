b4cb8bc4f46eb9aa6ce442ecd1dcc7de62efe019
Mariana Bedran Lesche <maribedran@gmail.com>
2018-01-18 16:51:26 -0300

    Add template dir to settings
    ----------------------------
    
    Adiciona diretório de templates à configurações

--- project/settings.py
+++ project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
