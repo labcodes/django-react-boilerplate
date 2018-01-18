ea1f5f21c457f9c64e5e7aacc5ef50b411dc4c2d
Mariana Bedran Lesche <maribedran@gmail.com>
2018-01-18 16:59:04 -0300

    Create view on root URL
    A template view to render the index template
    
    --------------------------------------------
    
    Cria view na raíz das URLs
    
    Uma template view para renderizar o template do index

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='frontend/index.html')),
 ]
