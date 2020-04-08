5b5d4269d6ed78e8641ff1afcaa61d1685135ade
Luciano Ratamero <luciano@ratamero.com>
2020-04-08 12:45:03 -0300

    Add 404 component

--- react-app/src/js/Routes.js
+++ react-app/src/js/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./Header";
+import NoMatch404 from "./NoMatch404";
 
 const WelcomePage = React.lazy(() => import("./welcome"));
 const SampleNestedPage = React.lazy(() => import("./sample-nested-page"));
@@ -14,6 +15,7 @@ export default class Routes extends React.Component {
           <Switch>
             <Route path="/sample-nested-page/" component={SampleNestedPage} />
             <Route exact path="/" component={WelcomePage} />
+            <Route component={NoMatch404} />
           </Switch>
         </React.Suspense>
       </Router>
