9f76fd08d45785b0d87a2f7b24aef2e6b2e7bf8c
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2021-06-11 15:38:17 -0300

    Rename folders that export components. Use capitalized names. Refactor 'sample-nested-page' to 'SampleNestedPage' and 'welcome' to 'WelcomePage'

--- react-app/src/js/rootReducer.js
+++ react-app/src/js/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import welcomeReducers from "./welcome/reducers";
+import welcomeReducers from "./WelcomePage/reducers";
 
 export default combineReducers({
   welcome: welcomeReducers
