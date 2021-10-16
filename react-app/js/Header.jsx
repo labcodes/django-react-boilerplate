917905b9188370fae54ccae6886de57be801b6ef
Luciano Ratamero <luciano@ratamero.com>
2021-10-15 21:16:05 -0300

    Change Header.jsx to a functional component

--- react-app/js/Header.jsx
+++ react-app/js/Header.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-export default class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <h1>Django + React Boilerplate</h1>
-      </header>
-    );
-  }
+export default function Header() {
+  return (
+    <header>
+      <h1>Django + React Boilerplate</h1>
+    </header>
+  );
 }
